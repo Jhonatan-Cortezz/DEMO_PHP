cd2fdd86d15c4da0497e9cdb4e44a803a81680b4
Jhonatan-Cortezz <jhonatancortez708@gmail.com>
2020-05-10 16:39:49 -0600

    Creacion del directorio ITCA y ficheros PHP

---
+++ ITCA/amaya.php
@@ -0,0 +1,8 @@
+<?php
+	echo <<<EOT
+		Al principio que tome esta carrera se me dificulataba mucho ya que casi no entendia las clases, estube a punto de dejar de estudiar por que no me entraban los conceptos 
+		pero todo en esta vida cuesta y lo que más nos cuesta es lo que más se valora, comenze a decicarle mas tiempo de estudio mas horas de desvelo y poco a poco he despertado
+		una curiosidad de seguir aprendiendo más y aqui estoy no soy un experto ni el mejor alumno pero estamos haciendo el intento de aprender.
+		EOT;
+
+?>
---
+++ ITCA/jhonatan.php
@@ -0,0 +1,9 @@
+<?php
+	echo <<<EOT
+		<h2>Mi nombre es Jhonatan Alexander Cortez Amaya</h2>
+		<p>Soy estudiante del segundo año de Técnico en ingenieria de Sistemas informáticos.
+		Particularmente Trabajar con git es un reto mas que me motiva a seguir mejorando como estudiante.<br>
+		Es una oportunidad para poder practicar lo que los desarrolladores hacen como profesionales en un trabajo, y me parece muy importante ya que de esta manera estamos trabajando 
+		de forma colaborativa con nuestros compañeros</p>
+		EOT;
+?>
