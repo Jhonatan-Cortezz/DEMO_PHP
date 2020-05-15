6ee13fee339538bb4a5c7af4765cad6d9099ea4e
Jhonatan-Cortezz <jhonatancortez708@gmail.com>
2020-05-15 10:29:47 -0600

    Se aplicara un estilo al archvio index.php

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Prueba de GitHub Forma gráfica</title>
+    <link rel="stylesheet" href="base.css">
 </head>
 <body>
     <h2>Introduzca los datos que se le piden a continuación</h2>
-    <form action="capturarDatos.php" method="post">
+    <form action="ITCA/capturarDatos.php" method="post">
         <label for="nombre">Nombre del usuario:</label>
         <input type="text" name="nombre" id="nombre">
         <label for="direccion">Dirección:</label>
